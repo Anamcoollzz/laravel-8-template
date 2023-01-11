1331b751dd08c60bcb426590bb3573933ccd9579
Anamcoollzz <hairulanam21@gmail.com>
2023-01-11 15:09:31 +0700

    fixing tooltip

--- resources/views/stisla/includes/forms/buttons/btn-danger.blade.php
+++ resources/views/stisla/includes/forms/buttons/btn-danger.blade.php
@@ -1,7 +1,7 @@
 @php
   $icon = $icon ?? 'fa fa-pencil-alt';
 @endphp
-<a class="btn btn-sm btn-danger @if ($icon ?? false) btn-icon icon-left @endif" href="{{ $link }}" data-toggle="tooltip" title="{{ $title ?? __('Danger') }}">
+<a class="btn btn-sm btn-danger @if ($icon ?? false) btn-icon icon-left @endif" href="{{ $link }}" data-toggle="tooltip" title="{{ $tooltip ?? ($title ?? __('Danger')) }}">
   @if ($icon ?? false)
     <i class="{{ $icon }}"></i>
   @endif
