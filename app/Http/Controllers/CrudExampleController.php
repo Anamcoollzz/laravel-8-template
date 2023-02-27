fc2c1532d73161bb572669257b4e86d1dd92cfef
Anamcoollzz <hairulanam21@gmail.com>
2023-02-27 13:47:37 +0700

    yajra: merapikan code datatable

--- app/Http/Controllers/CrudExampleController.php
+++ app/Http/Controllers/CrudExampleController.php
@@ -13,7 +13,6 @@ use App\Services\FileService;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Route;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
-use Yajra\DataTables\Facades\DataTables;
 
 class CrudExampleController extends Controller
 {
@@ -107,31 +106,7 @@ class CrudExampleController extends Controller
      */
     public function yajraAjax()
     {
-        $model = \App\Models\CrudExample::query();
-
-        return Datatables::of($model)
-            ->addIndexColumn()
-            ->editColumn('currency', '{{dollar($currency)}}')
-            ->editColumn('currency_idr', '{{rp($currency_idr)}}')
-            ->editColumn('select2_multiple', '{{implode(", ", $select2_multiple)}}')
-            ->editColumn('checkbox', '{{implode(", ", $checkbox)}}')
-            ->editColumn('checkbox2', '{{implode(", ", $checkbox2)}}')
-            ->editColumn('tags', 'stisla.crud-example.tags')
-            ->editColumn('file', 'stisla.crud-example.file')
-            ->editColumn('color', 'stisla.crud-example.color')
-            ->editColumn('created_at', '{{\Carbon\Carbon::parse($created_at)->addHour(7)->format("Y-m-d H:i:s")}}')
-            ->editColumn('updated_at', '{{\Carbon\Carbon::parse($updated_at)->addHour(7)->format("Y-m-d H:i:s")}}')
-            ->editColumn('action', function (CrudExample $crudExample) {
-                $user = auth()->user();
-                return view('stisla.crud-example.action', [
-                    'canUpdate' => $user->can('Contoh CRUD Ubah'),
-                    'canDetail' => $user->can('Contoh CRUD Detail'),
-                    'canDelete' => $user->can('Contoh CRUD Hapus'),
-                    'item'      => $crudExample,
-                ]);
-            })
-            ->rawColumns(['tags', 'file', 'color', 'action'])
-            ->make(true);
+        return $this->crudExampleRepository->getYajraDataTables();
     }
 
     /**
--- app/Repositories/CrudExampleRepository.php
+++ app/Repositories/CrudExampleRepository.php
@@ -3,6 +3,8 @@
 namespace App\Repositories;
 
 use App\Models\CrudExample;
+use Illuminate\Http\Response;
+use Yajra\DataTables\Facades\DataTables;
 
 class CrudExampleRepository extends Repository
 {
@@ -15,5 +17,37 @@ class CrudExampleRepository extends Repository
     public function __construct()
     {
         $this->model = new CrudExample();
+    }
+
+    /**
+     * get data for yajra datatables
+     *
+     * @return Response
+     */
+    public function getYajraDataTables()
+    {
+        return DataTables::of($this->query())
+            ->addIndexColumn()
+            ->editColumn('currency', '{{dollar($currency)}}')
+            ->editColumn('currency_idr', '{{rp($currency_idr)}}')
+            ->editColumn('select2_multiple', '{{implode(", ", $select2_multiple)}}')
+            ->editColumn('checkbox', '{{implode(", ", $checkbox)}}')
+            ->editColumn('checkbox2', '{{implode(", ", $checkbox2)}}')
+            ->editColumn('tags', 'stisla.crud-example.tags')
+            ->editColumn('file', 'stisla.crud-example.file')
+            ->editColumn('color', 'stisla.crud-example.color')
+            ->editColumn('created_at', '{{\Carbon\Carbon::parse($created_at)->addHour(7)->format("Y-m-d H:i:s")}}')
+            ->editColumn('updated_at', '{{\Carbon\Carbon::parse($updated_at)->addHour(7)->format("Y-m-d H:i:s")}}')
+            ->editColumn('action', function (CrudExample $crudExample) {
+                $user = auth()->user();
+                return view('stisla.crud-example.action', [
+                    'canUpdate' => $user->can('Contoh CRUD Ubah'),
+                    'canDetail' => $user->can('Contoh CRUD Detail'),
+                    'canDelete' => $user->can('Contoh CRUD Hapus'),
+                    'item'      => $crudExample,
+                ]);
+            })
+            ->rawColumns(['tags', 'file', 'color', 'action'])
+            ->make(true);
     }
 }
